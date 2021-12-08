6f036726732f597cb1967fa27b02904e03c66491
koulali <achraf.koulali@ncl.ac.uk>
2021-12-08 14:01:48 +0000

    update make script for coursework

--- make_ceg2722.sh
+++ make_ceg2722.sh
@@ -14,11 +14,11 @@ echo ""
 echo ""
 echo ""
 echo "=========================================================="
-echo ""
 echo "STEP 0: Check ID\n"
 echo -n "Enter your 5-digit ID " 
 read nuid
-echo ""
+
+
 echo "=========================================================="
 echo "STEP 1: Make sure you do have wget and curl"
 if ! [[ -x "$(command -v wget)" && -x "$(command -v curl)" ]]; then
@@ -27,9 +27,9 @@ if ! [[ -x "$(command -v wget)" && -x "$(command -v curl)" ]]; then
 else
 	echo "Great, lets go"
 fi
-echo ""
+
+
 echo "=========================================================="
-echo ""
 echo "STEP 2: clone CEG2722 \n"
 if ! [[ -x "$(command -v git)" ]]; then
 	sudo apt install git
@@ -37,51 +37,38 @@ else
 	echo "Great, git is already here"
 fi
 git clone https://github.com/koulali/ceg2722.git
-cd ~/
-echo ""
-echo "=========================================================="
-echo ""
-echo "STEP 3: Setup your bash profile\n"
 if [[ ! -d "~/bin" ]]; then
     mkdir ~/bin
 fi 
-echo "export PATH=$PATH:~/bin/" >> ~/.bashrc
-echo " done..."
+
+
 echo "=========================================================="
-echo ""
-echo "STEP 4: Download TEQC\n"
+echo "STEP 3: Download TEQC\n"
 wget https://www.unavco.org/software/data-processing/teqc/development/teqc_CentOSLx86_64d.zip
 unzip teqc_CentOSLx86_64d.zip
 mv teqc ~/bin/
 rm -f teqc_CentOSLx86_64d.zip
-echo " done..."
+
+
 echo "=========================================================="
-echo ""
-echo "STEP 5: Download CRX2RNX\n"
+echo "STEP 4: Setup CRX2RNX\n"
 mv ~/ceg2722/crx2rnx ~/bin/
 chmod +x ~/bin/crx2rnx
-echo " done..."
+
+
 echo "=========================================================="
-echo ""
-echo "STEP 6: Source your profile\n"
+echo "STEP 5: Setup your bash profile\n"
+if [[ ! -d "~/bin" ]]; then
+    mkdir ~/bin
+fi 
+echo "export PATH=$PATH:~/bin/" >> ~/.bashrc
+
+
+echo "=========================================================="
+echo "STEP 6: Change prompt\n"
 echo "export PS1='\h:${nuid}:\w\$ '" >> .bashrc
 echo "$USER" > ~/.userinfo
+
 source ~/.bashrc
-echo " done..."
-echo "=========================================================="
-echo ""
-echo "STEP 7: Create files for Practicals 1 and 2 \n"
-echo ""
-uname -a > ~/ceg2722/practical/.deskinfo
-for i in {001..100}; do
-	touch ~/ceg2722/practical/prac01/file${i}a.dat
-	touch ~/ceg2722/practical/prac01/file${i}b.dat
-	echo "this is file number: "${i}" a" >> ~/ceg2722/practical/prac01/file${i}a.dat
-	echo "this is file number: "${i}" b" >> ~/ceg2722/practical/prac01/file${i}b.dat
-done
-echo "=========================================================="
-echo ""
-source ~/.bashrc
+
 echo "That's All Folks!"
-
-
